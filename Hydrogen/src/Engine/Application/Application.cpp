a8edcdb3633e108246a28bcf49a496215a4efe5d
Kaustubh Narkhede <narkhedekaustubh13@gmail.com>
2022-08-18 01:17:48 +0530

    triangle

--- Hydrogen/src/Engine/Application/Application.cpp
+++ Hydrogen/src/Engine/Application/Application.cpp
@@ -46,7 +46,7 @@ namespace Hydrogen
 
 		for (auto it = m_LayerStack.end(); it != m_LayerStack.begin(); )
 		{
-			H2_CORE_INFO(e);
+			// H2_CORE_INFO(e);
 			(*--it)->OnEvent(e);
 			if (e.IsHandled())
 				break;
@@ -55,11 +55,41 @@ namespace Hydrogen
 
 	void Application::Run()
 	{
+		/*------Starter Code---------------*/
+		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		
+		glGenVertexArrays(1, &m_VertexArray);
+		glBindVertexArray(m_VertexArray);
+
+		glGenBuffers(1, &m_VertexBuffer);
+		glBindBuffer(GL_ARRAY_BUFFER, m_VertexBuffer);
+
+		float vertices[3 * 3] = {
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.0f,  0.5f, 0.0f
+		};
+
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+
+		glGenBuffers(1, &m_IndexBuffer);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IndexBuffer);
+
+		unsigned int indices[3] = { 0, 1, 2 };
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+		/*--------Starter Code End-----------*/
+
 		while (m_Running)
 		{
-			glClearColor(0.0f, 0.1f, 0.0f, 1.0f);
+			glClearColor(0.1f, 0.1f, 0.1f, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
-			
+
+			glBindVertexArray(m_VertexArray);
+			glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
+
 			for (Layer* l : m_LayerStack)
 				l->OnUpdate();
 
--- Sandbox/src/SandboxApplication.cpp
+++ Sandbox/src/SandboxApplication.cpp
@@ -23,10 +23,10 @@ public:
 	{
 		if (e.GetEventType() == Hydrogen::Events::EventType::KeyPressed)
 		{
-			Hydrogen::Events::KeyPressed& key_event = static_cast<Hydrogen::Events::KeyPressed&>(e);
-			if (key_event.GetKeyCode() == H2_KEY_TAB)
-				H2_TRACE("Tab Key Pressed");
-			H2_TRACE("{0}", (char)key_event.GetKeyCode());
+			// Hydrogen::Events::KeyPressed& key_event = static_cast<Hydrogen::Events::KeyPressed&>(e);
+			// if (key_event.GetKeyCode() == H2_KEY_TAB)
+			// 	H2_TRACE("Tab Key Pressed");
+			// H2_TRACE("{0}", (char)key_event.GetKeyCode());
 		}
 	}
 };
