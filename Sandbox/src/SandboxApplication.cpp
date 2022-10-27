5b34ebf478c9fb2d74f0a75b0aab155de2bf4398
Kaustubh Narkhede <narkhedekaustubh13@gmail.com>
2022-10-28 01:51:11 +0530

    added new test object (square)

--- Sandbox/src/SandboxApplication.cpp
+++ Sandbox/src/SandboxApplication.cpp
@@ -4,7 +4,13 @@
 ExampleLayer::ExampleLayer()
 	: Hydrogen::Layer("Example"), m_OrthoCamera(-1.0f, 1.0f, -1.0f, 1.0f), m_CameraPosition(0.0f)
 {
-	m_VertexArray.reset(Hydrogen::VertexArray::Create());
+	setupTraingle();
+	setupSqaure();
+}
+
+void ExampleLayer::setupTraingle()
+{
+	m_TriangleVA.reset(Hydrogen::VertexArray::Create());
 
 	float vertices[3 * 3] = {
 		-0.5f, -0.5f, 0.0f,
@@ -22,14 +28,40 @@ ExampleLayer::ExampleLayer()
 		vertexBuffer->SetLayout(Hydrogen::BufferLayout(layout));
 	}
 
-	m_VertexArray->AddVertexBuffer(vertexBuffer);
+	m_TriangleVA->AddVertexBuffer(vertexBuffer);
 
 	uint32_t indices[3] = { 0, 1, 2 };
 	std::shared_ptr<Hydrogen::IndexBuffer> indexBuffer;
 	indexBuffer.reset(Hydrogen::IndexBuffer::Create(indices, sizeof(indices) / sizeof(indices[0])));
-	m_VertexArray->SetIndexBuffer(indexBuffer);
+	m_TriangleVA->SetIndexBuffer(indexBuffer);
 
-	m_Shader.reset(new Hydrogen::Shader("../Hydrogen/res/Shaders/vertex_shader.glsl", "../Hydrogen/res/Shaders/fragment_shader.glsl", "Test"));
+	m_TriangleShader.reset(new Hydrogen::Shader("../Hydrogen/res/Shaders/triangle_vs.glsl", "../Hydrogen/res/Shaders/triangle_fs.glsl", "TriangleTest"));
+}
+
+void ExampleLayer::setupSqaure()
+{
+	m_SquareVA.reset(Hydrogen::VertexArray::Create());
+	
+	float squareVertices[3 * 4] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.5f,  0.5f, 0.0f,
+		-0.5f,  0.5f, 0.0f
+	};
+
+	std::shared_ptr<Hydrogen::VertexBuffer> squareVB;
+	squareVB.reset(Hydrogen::VertexBuffer::Create(squareVertices, sizeof(squareVertices)));
+	squareVB->SetLayout({
+		{Hydrogen::ShaderDataType::Float3, "a_Position"}
+	});
+	m_SquareVA->AddVertexBuffer(squareVB);
+
+	uint32_t squareIndices[6] = { 0, 1, 2, 2, 3, 0 };
+	std::shared_ptr<Hydrogen::IndexBuffer> squareIB;
+	squareIB.reset(Hydrogen::IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(squareIndices[0])));
+	m_SquareVA->SetIndexBuffer(squareIB);
+	
+	m_SquareShader.reset(new Hydrogen::Shader("../Hydrogen/res/Shaders/square_vs.glsl", "../Hydrogen/res/Shaders/square_fs.glsl", "SqaureTest"));
 }
 
 void ExampleLayer::OnUpdate(Hydrogen::Timestep ts)
@@ -49,8 +81,17 @@ void ExampleLayer::OnUpdate(Hydrogen::Timestep ts)
 	m_OrthoCamera.SetPosition(m_CameraPosition);
 	m_OrthoCamera.SetRotation(0.0f);
 
-	m_VertexArray->Bind();
-	Hydrogen::Renderer::Submit(m_VertexArray, m_Shader);
+	glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+	for (int y = 0; y < 20; y++)
+	{
+		for (int x = 0; x < 20; x++)
+		{
+			glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+			glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+			Hydrogen::Renderer::Submit(m_SquareVA, m_SquareShader, transform);
+		}
+	}
 
 	Hydrogen::Renderer::EndScene();
 }
--- Sandbox/src/SandboxApplication.hpp
+++ Sandbox/src/SandboxApplication.hpp
@@ -12,12 +12,19 @@ public:
 	void OnEvent(Hydrogen::Events::Event& e) override;
 
 private:
-	std::shared_ptr<Hydrogen::VertexArray> m_VertexArray;
-	std::shared_ptr<Hydrogen::Shader> m_Shader;
+	std::shared_ptr<Hydrogen::VertexArray> m_TriangleVA;
+	std::shared_ptr<Hydrogen::Shader> m_TriangleShader;
+	
+	std::shared_ptr<Hydrogen::VertexArray> m_SquareVA;
+	std::shared_ptr<Hydrogen::Shader> m_SquareShader;
 
 	Hydrogen::OrthographicCamera m_OrthoCamera;
 	glm::vec3 m_CameraPosition;
-	glm::float32 m_CameraSpeed{0.1f};
+	glm::float32 m_CameraSpeed{0.5f};
+
+private:
+	void setupTraingle();
+	void setupSqaure();
 };
 
 class Sandbox
